a7cadbd6726d4b5bfd16b7addbc20b916f7e2bf5
mohamedosama2074 <iosengks74@gmail.com>
2019-08-18 22:01:03 +0200

    UITextView

--- UITextView/ViewController.swift
+++ UITextView/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextViewDelegate{
 
+    @IBOutlet weak var labelCount: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textView.delegate = self
+        
+        
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        super.touchesBegan(touches, with: event)
+
+        self.view.endEditing(true)
+        
+    }
+
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        
+        textView.backgroundColor = UIColor.lightGray
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        
+        textView.backgroundColor = UIColor.white
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        labelCount.text = "\(textView.text.count)"
+        
+       
+        return true
+        
+    }
+//    
+//    func UpdateTextView(notification : Notification) {
+//        
+//        let userinfo = notification.userInfo
+//        
+//        let KeyboardEndFrameFromScreenCoordinates = (userinfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+//        
+//        let keyboardEndFrame = self.view.convert(
+//        
+//        
+//        
+//    }
+    
+
+
 
 
 }
-
